2089f4be907ed6d97115a9f4d23865cf4ebf05ef
Tejaswi <tejaswi.shetty08@gmail.com>
2018-05-14 14:25:39 +0530

    first commit

---
+++ src/test/java/info/seleniumcucumber/userStepDefintions/TestRunner.java
@@ -0,0 +1,19 @@
+package info.seleniumcucumber.userStepDefintions;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		tags = "@Regression",
+        features = "src/main/java/com/cucumber/features",
+        glue = "com.cucumber.stepdef",
+        plugin = { "html:target/cucumberHtmlReport",
+                    "pretty",
+                } 
+
+        )
+public class TestRunner {
+}
